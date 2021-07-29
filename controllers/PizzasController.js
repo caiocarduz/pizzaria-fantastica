bbb5fcba07f06fdebe2deb02dd9b1f7c395d11e5
caio <caio.carduz@gmail.com>
2021-07-28 22:08:45 -0300

    controller show adicionado

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,10 @@
+const pizzas = require('../database/Pizzas.json');
+
 module.exports = {
 	index: (req, res)=>{
-		res.render("index");
+		res.render("index", {pizzas: pizzas});
+	},
+	show: (req, res) =>{
+		res.render("pizza", {pizzas: pizzas.find(pizza => pizzas.id == req.params.id)})
 	}
 }
